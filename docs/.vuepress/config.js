7f6769ad88c5c05c9d29ae1ddbf51f3cd954ff40
biansj <biansj@yunxi.tv>
2021-10-24 23:09:36 +0800

    feat: vue press

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,45 @@
 module.exports = {
-  base: "/element-admin-docs/",
-  title: "ELEMENT-ADMIN",
-  description: "一个简单快速便捷易上手的 VUE ELEMENT 后台管理系统",
+  // base: "/element-admin-docs/",
+  title: "Element-Admin",
+  description: "一个简单的 Vue Element 后台管理系统",
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "/logo.png",
+      },
+    ],
+  ],
+  themeConfig: {
+    logo: "/logo.png",
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "指南", link: "/guide/" },
+      { text: "常见问题", link: "/problem/" },
+      { text: "更新日志", link: "/version/" },
+      {
+        text: "源码下载",
+        link: "https://github.com/sunshinemoment/element-admin",
+      },
+    ],
+    sidebarDepth: 2,
+    sidebar: {
+      "/guide/": [
+        {
+          title: "指南",
+          collapsable: false,
+          children: ["/guide/", "/guide/know"],
+        },
+      ],
+      "/problem/": ["/problem/"],
+      "/version/": [
+        {
+          title: "更新日志",
+          collapsable: false,
+          children: ["/version/", "/version/V1.0.0"],
+        },
+      ],
+    },
+  },
 };
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,13 @@
-# ELEMENT-ADMIN
+---
+home: true
+heroImage: /logo.png
+actionText: 快速开始 →
+actionLink: /guide/
+features:
+  - title: 技术栈
+    details: Vue 和 Element UI。
+  - title: 简单易上手
+    details: 模板化开发
+  - title: 高效率
+    details: 项目简单可配，内置代码生成器
+---
